129cf0cf851ffc30195146a9212e88d010779cd7
muzzamil176 <139631310+muzzamil176@users.noreply.github.com>
2023-07-15 19:52:10 +0500

    Add files via upload

---
+++ work.js
@@ -0,0 +1,39 @@
+{
+    var nam = "Eric";
+    console.log(nam, "  Hello Eric would you like to learn some Python today?"); //msg print
+    var nam1 = "Muzzamil";
+    console.log("Lowercase:muzzamil\nUppercase:MUZZAMIL\nTitlecase:MuzzAmil"); //personal name
+    console.log("As JohnCena said, “Live fast, fight hard, no regrets!” "); //quote
+    var famous_person = "QuaieAzam";
+    var message = "'No nation can rise to the height of glory unless your women are side by side with you'";
+    console.log("As ", famous_person, "said,", message);
+    console.log("\t \n      \t Muzzamil \t\n"); //whitespace
+    console.log("Muzzamil");
+    var addition = 2 + 6;
+    var multipication = 2 * 4;
+    var subtraction = 16 - 8;
+    var division = 16 / 2;
+    console.log("addition is ", addition, "\nsubttraction is", subtraction, "\nmultipication is", multipication, "\ndivision is ", division);
+    console.log(5 + 3);
+    console.log(5 + 3);
+    console.log(5 + 3);
+    console.log(5 + 3);
+    var num = "69";
+    console.log("My Favourite number is ", num);
+    var friends = ["hasaan", "mohsin", "zeeshan", "hamza"]; //storing name in array of friends and prining greetings
+    console.log(friends[0], "how are you bro");
+    console.log(friends[1], "how are you bro");
+    console.log(friends[2], "how are you bro");
+    console.log(friends[3], "how are you bro");
+    var fav_transport = ["cd70", "hondaGLI", "LAMBORGHINI"]; //array for my fav transport
+    console.log("I want to own ", fav_transport[0]);
+    console.log("I want to own ", fav_transport[1]);
+    console.log("I want to own ", fav_transport[2]);
+    var guest = ["brock", "rock", "randy", "undertaker"]; //inviting friends to dinner
+    console.log("I want to invite you to a dinner this saturday", guest[0]);
+    console.log("I want to invite you to a dinner this saturday", guest[1]);
+    console.log("I want to invite you to a dinner this saturday", guest[2]);
+    console.log("I want to invite you to a dinner this saturday", guest[3]);
+    var invites = " john cena";
+    console.log("you are invited to a dinner at saturday night", invites); //invitng replacement to dinner 
+}
